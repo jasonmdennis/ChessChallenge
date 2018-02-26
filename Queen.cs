e0c90b244e42bcd6e096cc6dc45de7d35793b71b
jasonmdennis <jasonmdennis@gmail.com>
2018-02-25 16:10:23 -0800

    Add files via upload

---
+++ Queen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessMoves.BRL
+{
+    class Queen : Pieces
+    {
+        private static Tuple<int, int> position = Tuple.Create(0, 0);
+        string orginalPostion = "";
+        string moves = "";
+        public override string GetMoves(string startPosition)
+        {
+            orginalPostion = startPosition;
+            position = getPosition(startPosition);
+
+            moves = standardHorizontalMoves(startPosition, moves);
+            moves = standardVerticalMoves(startPosition, moves);
+            moves = standardDiagonalMoves(startPosition, moves);
+
+            return moves;
+        }
+
+        protected override void customDiagonalMoves()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void customHorizontalMoves()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void customVerticalMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
